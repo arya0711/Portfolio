7fe1f92e99dd9ae5886c9b9f64c0aa1c1fb400e6
arya0711 <141263482+arya0711@users.noreply.github.com>
2023-08-02 22:33:47 +0530

    Add files via upload

---
+++ Portfolio-Arya Parolia/info.js
@@ -0,0 +1,11 @@
+var cc = document.getElementById("app_widget_profile_card_btn");
+function app_widget_profile_card(){
+var ct = document.getElementById("app_widget_profile_card_img");
+  ct.style.top = "260px"
+  cc.style.display = "block";
+}
+function app_widget_profile_card_out(){
+var ct = document.getElementById("app_widget_profile_card_img");
+  ct.style.top = "-40px"
+  cc.style.display = "none"
+}
